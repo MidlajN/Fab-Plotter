440150211b1c2f1d58917e8fa1f30d453c3c191e
MidlajN <midlaj4n@gmail.com>
2024-01-28 15:32:16 +0530

    Update : Display SVG function and UI updated

--- main.js
+++ main.js
@@ -1,27 +1,94 @@
-import './style.css'
-// import javascriptLogo from './javascript.svg'
-// import viteLogo from '/plotter.svg'
-// import { setupCounter } from './counter.js'
+import './style.css';
+import { svg2gcode } from 'svg-to-gcode/svg2gcode';
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
 
-// setupCounter(document.querySelector('#counter'))
 document.getElementById('gcode').onchange =()=>{
   console.log(document.getElementById('gcode').value)
 }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const dropArea = document.getElementById('dropArea');
+  const svgInput = document.getElementById('svgInput');
+  const svgInputDiv = document.getElementById('drop');
+
+  const zoomContainer = document.getElementById('result');
+  const zoomedContent = document.getElementById('zoomContainer');
+
+  dropArea.addEventListener('dragenter', (e) => {
+    e.preventDefault();
+    dropArea.style.boxShadow = 'rgb(0 159 255 / 50%) 0px 0px 16px 8px inset';
+    // dropArea.style.cursor = 'pointer';
+  })
+  dropArea.addEventListener('dragleave', (e) => {
+    e.preventDefault();
+    dropArea.style.boxShadow = 'inset 0px 0px 7px 5px #0000001f';
+    svgInputDiv.style.opacity = '1'
+  })
+  dropArea.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    svgInputDiv.style.opacity = '0.5'
+  })
+  dropArea.addEventListener('drop', (e) => {
+    e.preventDefault();
+    dropArea.style.boxShadow = 'inset 0px 0px 7px 5px #0000001f';
+    
+    const svgFiles = e.dataTransfer.files;
+    displaySvg(svgFiles[0])
+
+    // toggleButtonState();
+    // initializeGerberToSVG(files);
+  })
+
+  svgInput.addEventListener('change', (e) => {
+    const svgFiles = e.target.files;
+    displaySvg(svgFiles[0])
+    console.log('FILES ::', svgFiles[0]) 
+  })
+
+
+
+  // --------------------------- Zoom The Layer Using Mouse Wheel ---------------------------
+  let zoomLevel = 1;
+  const maxZoomLevel = 4
+
+  zoomContainer.addEventListener('wheel', (event) => {
+    event.preventDefault();
+
+    const zoomFactor = event.deltaY > 0 ? 0.9 : 1.1; // Adjust the zoom speed
+
+    const rect = zoomedContent.getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
+
+    const offsetX = (mouseX / rect.width) * 100;
+    const offsetY = (mouseY / rect.height) * 100;
+
+    const newZoomLevel = zoomLevel * zoomFactor;
+    zoomLevel = Math.max(1, Math.min(maxZoomLevel, newZoomLevel)); // Ensure zoom doesn't go below 100%
+
+    zoomedContent.style.transformOrigin = `${offsetX}% ${offsetY}%`;
+    zoomedContent.style.transform = `scale(${zoomLevel})`;
+  });
+
+  function displaySvg(file){
+    if (file) {
+      const reader = new FileReader();
+  
+      reader.onload = (e) => {
+        const svgContent = e.target.result;
+        console.log(svgContent)
+        const img = document.createElement('img');
+        img.src = svgContent
+        zoomedContent.appendChild(img);
+
+        zoomContainer.style.opacity = '1';
+        svgInputDiv.style.display = 'none';
+      }
+      reader.readAsDataURL(file)
+    }
+  }
+})
+
+
+
