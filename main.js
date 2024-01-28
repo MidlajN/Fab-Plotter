f6053c36d1e6c01d7970ff937b943e3005a4365b
MidlajN <midlaj4n@gmail.com>
2024-01-28 23:27:10 +0530

    Serial Port Connect through Webserial API implemented

--- main.js
+++ main.js
@@ -1,23 +1,19 @@
 import './style.css';
 import { Converter } from 'svg-to-gcode';
 import { optimize } from 'svgo';
-
-
-document.getElementById('gcode').onchange =()=>{
-  console.log(document.getElementById('gcode').value)
-}
+import { requestSerialPort, closeSerialPort } from './serial';
 
 
 document.addEventListener('DOMContentLoaded', function () {
+  let zoomLevel = 1;
+  const maxZoomLevel = 4;
+  let svgContent;
+  let gcodeArray = [];
+
+  // ------------------- Drag and Drop & Input Functions ----------------------
   const dropArea = document.getElementById('dropArea');
   const svgInput = document.getElementById('svgInput');
   const svgInputDiv = document.getElementById('drop');
-
-  const zoomContainer = document.getElementById('result');
-  const zoomedContent = document.getElementById('zoomContainer');
-
-  let svgContent;
-  let gcodeLines;
 
   dropArea.addEventListener('dragenter', (e) => {
     e.preventDefault();
@@ -48,10 +44,9 @@ document.addEventListener('DOMContentLoaded', function () {
   })
 
 
-
   // --------------------------- Zoom The Layer Using Mouse Wheel ---------------------------
-  let zoomLevel = 1;
-  const maxZoomLevel = 4
+  const zoomContainer = document.getElementById('result');
+  const zoomedContent = document.getElementById('zoomContainer');
 
   zoomContainer.addEventListener('wheel', (event) => {
     event.preventDefault();
@@ -87,10 +82,15 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
 
+  // ---------------------- G-Code Generation From SVG ------------------------- 
   const gcodeBtn = document.getElementById('generateGcode');
-  gcodeBtn.addEventListener('click', async () => {
+  // const clrBtn = document.getElementById('clearCode');
+  const textarea = document.getElementById('gcode')
+  
+  gcodeBtn.addEventListener('click', () => {
     const result = optimize(svgContent);
     const optimizedSvg = result.data; // Optionally the optimized SVG can be used
+    let gcode;
     
     const zOffset = 4;
     const feedRate = document.getElementById('feedRate').value;
@@ -98,21 +98,65 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const settings = {
       zOffset : zOffset,
-      feedRate : feedRate,
-      seekRate : seekRate
+      feedRate : 100,
+      seekRate : 200
     }
-    const converter = new Converter(settings)
+    const converter = new Converter(settings);
 
-    const gcode = await converter.convert(svgContent)
-    const textarea = document.getElementById('gcode')
-    gcodeBtn.setAttribute('disabled', true)
-    textarea.value = gcode[0];
-    // textarea.removeAttribute('disabled')
-    textarea.style.color = 'gray';
+    converter.convert(svgContent).then((gcodes) => {
+      console.log('GCODE FROM CONVERTER ::::', gcodes)
+      gcode = gcodes[0];
+      gcodeBtn.setAttribute('disabled', true)
+      // textarea.removeAttribute('disabled')
 
-    gcodeLines = gcode[0].split('\n')
+      textarea.value = gcode;
+      textarea.style.color = 'gray';
+      const gcodeLines = gcode.split('\n');
+      gcodeLines.forEach(gcode => {
+        // remove unnecessary whitespace from each line 
+        const trimmedLine = gcode.trim(); 
+        if (trimmedLine !== "") {
+          gcodeArray.push(trimmedLine)
+        }
+      });
+    })
+
+    // // Clear G-Code Button
+    // clrBtn.style.color = 'red'
+    // clrBtn.removeAttribute('disabled');
+    // clrBtn.classList.add('clearCode');
   })
-  
+
+
+  // ------------ Button For Clearing the Existing G-Code from the textarea -----------
+  // clrBtn.addEventListener('click', () => {
+  //   document.getElementById('gcode').value = '';
+  //   document.getElementById('gcode').setAttribute('disabled', true);
+  //   clrBtn.setAttribute('disabled', true);
+  //   clrBtn.classList.remove('clearCode');
+  //   clrBtn.style.color = 'white';
+  //   gcodeBtn.removeAttribute('disabled')
+  //   console.log(gcodeLines)
+  // })
+
+
+  // ------------------- Connect & Send the G-Code to Plotter via Serial API -----------------------
+  const serialBtn = document.getElementById('connect');
+  serialBtn.addEventListener('click', () => {
+    console.log('Connect Btn', gcodeArray)
+    requestSerialPort().then(() => {
+      serialBtn.style.display = 'none';
+      document.getElementById('disconnect').style.display = 'block'
+    })
+  })
+
+  const closeSerialBtn = document.getElementById('disconnect');
+  closeSerialBtn.addEventListener('click', () => {
+    closeSerialPort().then(() => {
+      document.getElementById('connect').style.display = 'block';
+      document.getElementById('disconnect').style.display = 'none';
+    })
+  })
 })
 
 
